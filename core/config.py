1c61259f34bd7589e2e4823535ea89e7e4c3b83a
vzalxndr <vzalxndr.reserve@gmail.com>
2026-04-13 21:00:57 +0200

    feat: switch to groq api

--- core/config.py
+++ core/config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 bot_token = os.getenv("bot_token")
+groq_api_key = os.getenv("GROQ_API_KEY")
 
-if not bot_token:
-    raise valueerror("bot_token is not set in .env file")
+if not bot_token or not groq_api_key:
+    raise ValueError("API tokens are not set in .env file")
